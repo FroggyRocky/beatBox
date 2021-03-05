017b55cca41170c0579a1519f7fbd45c3a15c120
FroggyRocky <43971330+FroggyRocky@users.noreply.github.com>
2021-03-05 14:36:22 +0400

    usage of setTimeOut function

--- index.js
+++ index.js
@@ -4,18 +4,19 @@ const kch = new Audio("sounds/kch.mp3");
 const scratch = new Audio("sounds/scratch.mp3");
 const yo = new Audio("sounds/yo.mp3");
 const setBeat = new Audio("sounds/setbeat.mp3");
-
 for (var i = 0; i < document.querySelectorAll(".set button, spanJ").length; i++)
 
 { document.querySelectorAll(".set button")[i].addEventListener("click",
  function() { 
      var innerText = this.innerText; 
      keyBoard(innerText);
+     toggleDrum(innerText)
      }
      );
 }
 document.addEventListener("keypress", function (event){
     keyBoard(event.key);
+toggleDrum(event.key)
 })
 function keyBoard (keyBoard) {
     switch (keyBoard) {
@@ -39,3 +40,11 @@ function keyBoard (keyBoard) {
             break;
     }
 }
+function toggleDrum(key) {
+let active = document.querySelectorAll("." + key);
+active.forEach(element => {
+element.classList.add("key-active") 
+setTimeout(() => {
+    element.classList.remove("key-active") 
+}, 200);
+})}
